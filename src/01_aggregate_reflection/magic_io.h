228c950db004b7ac4fadb636f697c22cc28a94a7
Ruslan Manaev <manavrion@gmail.com>
2020-02-23 15:50:58 +0300

    Prepare 01_aggregate_reflection

---
+++ src/01_aggregate_reflection/magic_io.h
@@ -0,0 +1,32 @@
+#include <boost/pfr/detail/tie_from_structure_tuple.hpp>
+#include <iostream>
+
+#include "for_each.h"
+
+template <class... Ts>
+struct overloaded : Ts... {
+  using Ts::operator()...;
+};
+template <class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;  // not needed as of C++20
+
+namespace magic_io::magic {
+using boost::pfr::detail::tie_as_tuple;
+}  // namespace magic_io::magic
+
+namespace magic_io {
+
+template <typename Char, typename Traits, typename T>
+auto& operator<<(std::basic_ostream<Char, Traits>& out, const T& value) {
+  out << '{';
+  auto tie = magic::tie_as_tuple(value);
+  detail::for_each(tie,
+                   overloaded{[&](auto&& arg) { out << arg << ", "; },
+                              [&](const std::basic_string<Char, Traits>& arg) {
+                                out << "\"" << arg << "\", ";
+                              }});
+  out << '}';
+  return out;
+}
+
+}  // namespace magic_io
