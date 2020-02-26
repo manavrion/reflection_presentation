863cf5f04b7daebe04834b3f24955941293f9225
Ruslan Manaev <manavrion@gmail.com>
2020-02-26 21:10:47 +0300

    Update compiler

--- prepare_compiler.sh
+++ prepare_compiler.sh
@@ -4,6 +4,6 @@ cd $(dirname "$0")
 rm -rf compiler
 mkdir compiler
 cd compiler
-wget -nv https://github.com/manavrion/clang_hub/releases/download/v1.0/clang_of_lock3_compiler-explorer.tar.gz
-tar -xf clang_of_lock3_compiler-explorer.tar.gz
-rm clang_of_lock3_compiler-explorer.tar.gz
+wget -nv https://github.com/manavrion/clang_hub/releases/download/v1.1/clang_of_lock3_compiler_explorer.tar.gz
+tar -xf clang_of_lock3_compiler_explorer.tar.gz
+rm clang_of_lock3_compiler_explorer.tar.gz
